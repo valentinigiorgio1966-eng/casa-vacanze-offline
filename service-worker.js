bfab21d8e74181766a78ed483d476b8bb730a51d
valentinigiorgio1966-eng <valentinigiorgio1966@gmail.com>
2026-02-03 12:20:02 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,29 @@
-/* Casa Vacanze - Service Worker (GitHub Pages) */
-const CACHE_NAME = "casa-vacanze-cache-v1";
+const CACHE = "casavacanze-v1";
 const ASSETS = [
   "./",
   "./index.html",
-  "./indice.html",
-  "./logo.jpg",
   "./manifest.webmanifest",
-  "./manifesto.webmanifest",
   "./service-worker.js",
-  "./icons/icon-192x192.png",
-  "./icons/icon-256x256.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  if (req.method !== "GET") return;
-
   event.respondWith(
-    caches.match(req).then((cached) => {
-      if (cached) return cached;
-      return fetch(req).then((resp) => {
-        const copy = resp.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(()=>{});
-        return resp;
-      }).catch(() => caches.match("./index.html"));
-    })
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
